52311d73f094103154ddf44123ddfeba696a5a9f
blingz <binliangzhang@gmail.com>
2018-01-13 17:16:20 +0800

    support key access

--- test/test.js
+++ test/test.js
@@ -10,6 +10,12 @@ describe('json, test', () => {
       var json = fiw('json')
       var txt = json.minify(`{"a":1}/*abc*/`)
       assert.equal(txt, `{"a":1}`);
+
+      var json2 = fiw.json
+      var txt2 = json.minify(`{"a":1}/*abc*/`)
+      assert.equal(txt2, `{"a":1}`);
+
+      console.error(fiw.xx)
     });
 
 });
